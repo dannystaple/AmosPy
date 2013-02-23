1ee4df23aa6b614cb0c91240536266b92de4ccc1
Danny Staple <danny@orionrobots.co.uk>
2013-02-23 07:31:34 +0000

    Switch to pytest - simpler for now.

--- test_read_parse_toktab.py
+++ test_read_parse_toktab.py
@@ -1,8 +1,5 @@
-__author__ = 'stapled'
-
 from read_parse_toktab import process_similar
-from nose.tools import assert_equals
-from unittest import TestCase
+
 
 def test_process_similar():
     pairs = [
@@ -10,6 +7,4 @@ def test_process_similar():
         ('462', '$80,"I",-1', ('462+4 TkAd4:\tdc.w CAdd4-Tk,Synt-Tk', '+4 \tdc.b $80,"I",-1'))
     ]
     out = list(process_similar(pairs))
-    expected = '"ad","d"+$80,"I",-2'
-    assert_equals(out[1][1], '"ad","d"+$80,"I",-2')
-
+    assert(out[1][1] == '"ad","d"+$80,"I",-2')
